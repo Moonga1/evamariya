e8364b49390d92f5c0acd11c81bda22019269e25
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-11-30 16:55:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,7 +58,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker(get.SHAZ)
+        m=await message.reply_sticker(sticker=get.(SHAZ))
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -110,7 +110,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker(get.SHAZ)
+        m=await message.reply_sticker(sticker=get.(SHAZ))
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
