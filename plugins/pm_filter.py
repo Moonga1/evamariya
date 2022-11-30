6951c8c3662d040628360fa26d4245a513a574fd
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-11-30 19:14:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -782,9 +782,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
                   ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAEGn6BjhzNru_RMkSJI-Si3AZE45PbqEQACEwADwSQxMaTiEyebh2jwKwQ")
-        await asyncio.sleep(1)
-        await m.delete()
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
