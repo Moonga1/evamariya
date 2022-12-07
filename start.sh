79d2e5941370ca471976834bd6bb39735e07c683
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-12-07 17:33:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Moonga1/evamariya /
+  git clone https://github.com/Moonga1/evamariya /evamariya
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /evamariya
