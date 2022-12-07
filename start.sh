aec0e4582ba9ef561060f16614af823e504da4c4
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-12-07 17:32:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Moonga1/evamariya.git /evamariya
+  git clone https://github.com/Moonga1/evamariya /
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /evamariya
 fi
-cd /evamariya
+cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
 echo "Starting evamariya...."
 python3 bot.py
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
